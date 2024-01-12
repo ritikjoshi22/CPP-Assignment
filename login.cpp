28e339fec64b5d5fc59b80f2812b4a2c63af16bf
ritikjoshi22 <142025990+ritikjoshi22@users.noreply.github.com>
2024-01-12 17:37:18 +0545

    login changes

--- login.cpp
+++ login.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+class detail
+{
+    public:
+    string username;
+    string password;
+};
+
 int main()
 {
-    cout<<"Login page";
-    
+    detail u1;
+    cout<<"\t\t\t\t\t\tLogin page\n";
+    cout<<"Username:";
+    cin>>u1.username;
+    cout<<"Password:";
+    cin>>u1.password;
+    if(u1.username=="ritik" && u1.password=="ironman"){
+        cout<<"Access granted"<<endl;
+    }
+    else{
+        cout<<"Try Again"<<endl;
+    }
+    return 0;
 }
